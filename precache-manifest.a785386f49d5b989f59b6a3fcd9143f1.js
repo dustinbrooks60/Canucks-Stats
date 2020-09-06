de8fa17b728c0caf5dacf02030a3e75c2581481e
dustinbrooks60 <dbrooks49@my.bcit.ca>
2020-09-05 17:02:32 -0700

    Updates

---
+++ precache-manifest.a785386f49d5b989f59b6a3fcd9143f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74058c439fc5e87dc7ef8798eafcaa41",
+    "url": "/Canucks-Stats/index.html"
+  },
+  {
+    "revision": "27311fa3b51eace43ffd",
+    "url": "/Canucks-Stats/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "db709ee3f46fb32ba4de",
+    "url": "/Canucks-Stats/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "27311fa3b51eace43ffd",
+    "url": "/Canucks-Stats/static/js/2.95f50b89.chunk.js"
+  },
+  {
+    "revision": "86a36a32ea77366f8e1062ebfa4eedd0",
+    "url": "/Canucks-Stats/static/js/2.95f50b89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db709ee3f46fb32ba4de",
+    "url": "/Canucks-Stats/static/js/main.011edd3a.chunk.js"
+  },
+  {
+    "revision": "07d306b325a3e5ccea39",
+    "url": "/Canucks-Stats/static/js/runtime-main.d57838ad.js"
+  }
+]);
